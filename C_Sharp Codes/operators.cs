28b0f6d646156b1e07e4fdbe31c5183c239b21e2
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-13 23:29:01 +0530

    operators in c#

---
+++ C_Sharp Codes/operators.cs
@@ -0,0 +1,23 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace Csharp_programing
+//{
+//    internal class operators
+//    {
+//        public static void Main(string[] args)
+//        {
+//            Console.WriteLine("operators");
+//            Console.WriteLine();
+
+//            Console.WriteLine("addition is" + (10 + 12));
+//            Console.WriteLine("substraction is" + (10 - 5));
+//            Console.WriteLine("multiplication is" + (10 * 10));
+//            Console.WriteLine("division is" + (10 / 2));
+//            Console.WriteLine("modules is" + (10 % 10));
+
+
+//        }
+//    }
+//}
