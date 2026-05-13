9eefd09ede86045a70ff85562ff3810eb2e640cf
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-13 23:28:21 +0530

    datatype in c# with example in code

---
+++ C_Sharp Codes/Datatypes.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+   class Datatypes
+    {
+    public static void Main(string[] args)
+    {
+        //datatypes in c
+
+        //1) value type datatype
+
+        //numbers
+
+        byte b = 255;
+        Console.WriteLine("byte:" + b);
+
+        short s = 1234;
+        Console.WriteLine("short:"+s);
+
+        int i = 123456777;
+        Console.WriteLine("int:"+i);
+
+        long l = 319874024142;
+        Console.WriteLine("long is"+l);
+
+        // decimals
+
+        float f = 100;
+        Console.WriteLine("float:"+f);
+
+        double d = 12343.3;
+        Console.WriteLine("double:"+d);
+
+
+        decimal dc = 1232;
+        Console.WriteLine("decimal:" + dc);
+
+        //charaters
+
+        char c='A';
+        Console.WriteLine("char:"+ c);
+
+        bool isvalid=true;
+        Console.WriteLine("bool:"+ isvalid);
+
+       
+
+        //refrence type datatype
+
+        //string
+
+        //object
+
+        //array
+
+
+    }
+
+    }
+
