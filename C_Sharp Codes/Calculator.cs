d05fb62bc2756c520e3efb609ab29de018273887
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-14 21:29:37 +0530

    Console base calculator in c#

--- C_Sharp Codes/Calculator.cs
+++ C_Sharp Codes/Calculator.cs
@@ -1,36 +1,36 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 
-//   class Calculator
-//    {
+  class Calculator
+   {
 
-//       public static void Main(string[] args)
-//       {
-//        int a,b;
+      public static void Main(string[] args)
+      {
+       int a,b;
 
-//        Console.Write("enter first number");
-//        a= int.Parse(Console.ReadLine());
+       Console.Write("enter first number");
+       a= int.Parse(Console.ReadLine());
 
-//        Console.WriteLine();
+       Console.WriteLine();
 
-//        Console.Write("enter second number");
-//        b=int.Parse(Console.ReadLine());
+       Console.Write("enter second number");
+       b=int.Parse(Console.ReadLine());
 
-//        Console.WriteLine("the addition is:- "+(a + b));
-//        Console.WriteLine("the substration is:- " + (a - b));
-//        Console.WriteLine("the multiplication is:- " + (a * b));
-//        Console.WriteLine("the Division is:- " + (a / b));
-//        Console.WriteLine("the reminder is:- " + (a % b));
+       Console.WriteLine("the addition is:- "+(a + b));
+       Console.WriteLine("the substration is:- " + (a - b));
+       Console.WriteLine("the multiplication is:- " + (a * b));
+       Console.WriteLine("the Division is:- " + (a / b));
+       Console.WriteLine("the reminder is:- " + (a % b));
 
 
         
 
 
 
-//       }
+      }
 
 
-//    }
+   }
 
