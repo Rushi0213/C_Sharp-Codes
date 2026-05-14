f3d5c669b0de9e3aac86525adbcd78bb842a531f
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-14 21:34:33 +0530

    conditional statement if else

---
+++ C_Sharp Codes/ifElsestmt.cs
@@ -0,0 +1,25 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+
+//    class ifElsestmt
+//    {
+
+//  public static void Main(string[] args)
+//    {
+
+//        int age = int.Parse(Console.ReadLine());
+//        if(age>18)
+//        {
+//            Console.WriteLine("you are eligible for voting");
+
+//        }
+//        else
+//        {
+//            Console.WriteLine("you are not eligible for voting");
+//        }
+//    }
+
+//}
+
