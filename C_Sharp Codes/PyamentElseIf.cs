ae44af4725291377996833077c25e72955ea62f3
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-14 21:37:06 +0530

    payment method else if code

---
+++ C_Sharp Codes/PyamentElseIf.cs
@@ -0,0 +1,40 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+
+//     class PyamentElseIf
+//    {
+//    public static void Main(string[] args)
+//    {
+
+//        Console.WriteLine("select payment method");
+//        Console.WriteLine("UPI?");
+//     bool isUPI=bool.Parse(Console.ReadLine());
+
+//        Console.WriteLine("Credit?");
+//        bool isCredit = bool.Parse(Console.ReadLine());
+
+//        Console.WriteLine("Debit?");
+//        bool isDebit = bool.Parse(Console.ReadLine());
+
+
+//        if (isUPI)
+//        {
+//            Console.WriteLine("payment succesfull comeleted by UPI");
+//        }
+//        else if (isCredit)
+//        {
+//            Console.WriteLine("payment succesfull comeleted by Credit");
+//        }
+//        else if (isDebit)
+//        {
+//            Console.WriteLine("payment succesfull comeleted  by Debit");
+//        }
+//        else
+//        {
+//            Console.WriteLine("cash on delivery");
+//        }
+//    }
+//}
+
