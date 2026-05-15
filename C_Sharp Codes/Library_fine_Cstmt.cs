2462a387dfb15831dee0b55bd831d2c2c635456f
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-15 23:26:47 +0530

    Update Library_fine_Cstmt.cs

--- C_Sharp Codes/Library_fine_Cstmt.cs
+++ C_Sharp Codes/Library_fine_Cstmt.cs
@@ -1,35 +1,35 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Runtime.Intrinsics.Arm;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Intrinsics.Arm;
+using System.Text;
 
 
-//     class Library_fine_Cstmt
-//    {
-//   public static void Main(string[] args)
-//    {
-//        Console.WriteLine("enter the Fee");
-//        int days=Convert.ToInt32(Console.ReadLine());
+    class Library_fine_Cstmt
+   {
+  public static void Main(string[] args)
+   {
+       Console.WriteLine("enter the Fee");
+       int days=Convert.ToInt32(Console.ReadLine());
 
-//        if(days>20)
-//        {
-//            Console.WriteLine("you will py the fine 500rs");
-//        }
-//       else if (days > 10)
-//        {
-//            Console.WriteLine("you wil pay the fine 100rs");
-//        }
-//        else if (days > 5)
-//        {
-//            Console.WriteLine("you will pa y the fee 50rs");
-//        }
-//        else
-//        {
-//            Console.WriteLine("No file");
-//        }
+       if(days>20)
+       {
+           Console.WriteLine("you will py the fine 500rs");
+       }
+      else if (days > 10)
+       {
+           Console.WriteLine("you wil pay the fine 100rs");
+       }
+       else if (days > 5)
+       {
+           Console.WriteLine("you will pa y the fee 50rs");
+       }
+       else
+       {
+           Console.WriteLine("No file");
+       }
 
 
-//    }
+   }
 
-//}
+}
 
