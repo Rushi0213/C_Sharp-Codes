f2d57ebb8b9d6a6643b292731184f5a457526a18
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-15 23:20:46 +0530

    forget password code in c#

---
+++ C_Sharp Codes/Forgatepassword_Switchcase.cs
@@ -0,0 +1,37 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+
+//    class Forgatepassword_Switchcase
+//    {
+//   public static void Main(string[] args)
+//    {
+//        Console.WriteLine("forgate password using another way");
+//        Console.WriteLine("enter methods");
+//        string options=Console.ReadLine();
+
+//        switch(options)
+//        {
+//            case "Email":
+//                Console.WriteLine("login by email");
+//                break;
+
+//            case "Phone":
+//                Console.WriteLine("login by phone");
+//                break;
+
+//            case "Aadhar":
+//                Console.WriteLine("login by Aadhar");
+//                break;
+
+
+//            default:
+//                Console.WriteLine("invalid options");
+//                break ;
+
+//        }
+//    }
+
+//}
+
