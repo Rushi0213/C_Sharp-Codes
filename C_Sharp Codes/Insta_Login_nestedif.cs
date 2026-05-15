96d43cd57b1cd8f91cf30abc5a9c3bbd5cda336c
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-15 23:23:46 +0530

    insta login nested if statement

---
+++ C_Sharp Codes/Insta_Login_nestedif.cs
@@ -0,0 +1,37 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+
+//    class Insta_Login_nestedif
+//    {
+//  public static void Main(string[] args)
+//    {
+//        Console.WriteLine("instgram login and register process");
+
+//        bool IsReg= bool.Parse(Console.ReadLine());
+
+//        if(IsReg)
+//        {
+//            Console.WriteLine("enter username");
+//            string name= Console.ReadLine();
+//            Console.WriteLine("enter password");
+//            string password= Console.ReadLine();
+
+//            if(name=="rushi" && password=="rushi2123")
+//            {
+//                Console.WriteLine("login succesfull");
+//            }
+//            else
+//            {
+//                Console.WriteLine("login unsuccesfull");
+//            }
+
+//        }
+//        else
+//        {
+//            Console.WriteLine("create new account");
+//        }
+//    }
+//}
+
