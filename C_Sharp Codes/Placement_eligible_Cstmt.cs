e3c3a1b0c4139c8b41a92672aae4b28fa86229ab
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-15 23:26:00 +0530

    placement eligible  statement in c#

---
+++ C_Sharp Codes/Placement_eligible_Cstmt.cs
@@ -0,0 +1,24 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+
+//class Placement_eligible_Cstmt
+//{
+//    public static void Main(string[] args)
+//    {
+//        Console.WriteLine("enter the Cgpa");
+//        int cgpa = Convert.ToInt32(Console.ReadLine());
+//        Console.WriteLine("check the backlog criteria");
+//        bool NOBacklog = Convert.ToBoolean(Console.ReadLine());
+//        if (cgpa >= 7 && NOBacklog == true)
+//        {
+//            Console.WriteLine(" You are Eligible for placement");
+//        }
+//        else
+//        {
+//            Console.WriteLine("you are not eligible for placement");
+//        }
+//    }
+//}
+
