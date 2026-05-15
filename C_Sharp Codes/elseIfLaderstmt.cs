9baa0cf6fc8e822c0834723e7c5a846110201974
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-15 23:30:40 +0530

    Update elseIfLaderstmt.cs

--- C_Sharp Codes/elseIfLaderstmt.cs
+++ C_Sharp Codes/elseIfLaderstmt.cs
@@ -1,36 +1,36 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 
-//    internal class elseIfLaderstmt
-//    {
-//  public  static void Main(string[] args)
-//    {
-//        Console.Write("enter your marks");
-//      int marks=Convert.ToInt32(Console.ReadLine());
+   internal class elseIfLaderstmt
+   {
+ public  static void Main(string[] args)
+   {
+       Console.Write("enter your marks");
+     int marks=Convert.ToInt32(Console.ReadLine());
 
-//        if(marks>90)
-//        {
-//            Console.WriteLine("excellent");
-//        }
-//        else if(marks>70)
-//        {
-//            Console.WriteLine("Good");
-//        }
-//        else if(marks>50)
-//        {
-//            Console.WriteLine("average");
-//        }
-//        else if(marks>35)
-//        {
-//            Console.WriteLine("pass");
-//        }
-//        else
-//        {
-//            Console.WriteLine("you are fail");
-//        }
+       if(marks>90)
+       {
+           Console.WriteLine("excellent");
+       }
+       else if(marks>70)
+       {
+           Console.WriteLine("Good");
+       }
+       else if(marks>50)
+       {
+           Console.WriteLine("average");
+       }
+       else if(marks>35)
+       {
+           Console.WriteLine("pass");
+       }
+       else
+       {
+           Console.WriteLine("you are fail");
+       }
         
-//    }
-//}
+   }
+}
 
