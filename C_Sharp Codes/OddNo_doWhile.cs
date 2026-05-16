8e55069d5f0defed4a8288dbada1338b744340a1
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-16 23:42:03 +0530

    odd no code in while loop c#

---
+++ C_Sharp Codes/OddNo_doWhile.cs
@@ -0,0 +1,25 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+
+//     class OddNo_doWhile
+//    {
+//  public  static void Main(string[] args)
+//    {
+//        Console.WriteLine("enter the no");
+//        //int no=Convert.ToInt32(Console.ReadLine());
+//        int num = 1;
+//        do
+//        {
+//            if (num % 2 != 0)
+//            {
+//                Console.WriteLine(num);
+               
+//            }
+//            num++;
+//        }
+//        while (num<=50);
+//    }
+//}
+
