a38838fbe341a4bf06232018cdf14a10f9a221d1
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-16 23:42:44 +0530

    count digit  code in while loop c#

---
+++ C_Sharp Codes/CountDigit_whileloop.cs
@@ -0,0 +1,23 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+
+//     class CountDigit_whileloop
+//    {
+//   public static void Main(string[] args)
+//    {
+//        Console.WriteLine("enter the number");
+//        int no=Convert.ToInt32(Console.ReadLine());
+//        int count = 0;
+
+//        while(no>0)
+//        {
+//            count++;
+//            no = no / 10;
+
+//        }
+//        Console.WriteLine(count);
+//    }
+//}
+
