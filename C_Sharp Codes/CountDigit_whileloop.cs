65d83c2cebc756faf7617c47ea81287eacb0d793
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-16 23:45:04 +0530

    Update CountDigit_whileloop.cs

--- C_Sharp Codes/CountDigit_whileloop.cs
+++ C_Sharp Codes/CountDigit_whileloop.cs
@@ -1,23 +1,23 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 
-//     class CountDigit_whileloop
-//    {
-//   public static void Main(string[] args)
-//    {
-//        Console.WriteLine("enter the number");
-//        int no=Convert.ToInt32(Console.ReadLine());
-//        int count = 0;
+    class CountDigit_whileloop
+   {
+  public static void Main(string[] args)
+   {
+       Console.WriteLine("enter the number");
+       int no=Convert.ToInt32(Console.ReadLine());
+       int count = 0;
 
-//        while(no>0)
-//        {
-//            count++;
-//            no = no / 10;
+       while(no>0)
+       {
+           count++;
+           no = no / 10;
 
-//        }
-//        Console.WriteLine(count);
-//    }
-//}
+       }
+       Console.WriteLine(count);
+   }
+}
 
