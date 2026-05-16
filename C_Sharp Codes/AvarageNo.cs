dcadecb4658e5473da2696a92e02a786408a7680
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-16 23:40:32 +0530

    Update AvarageNo.cs

--- C_Sharp Codes/AvarageNo.cs
+++ C_Sharp Codes/AvarageNo.cs
@@ -1,26 +1,26 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 
-//    class AvarageNo
-//    {
-//     public static void Main(string[] args)
-//    {
+   class AvarageNo
+   {
+    public static void Main(string[] args)
+   {
 
-//        Console.WriteLine("Enter four numbers");
+       Console.WriteLine("Enter four numbers");
 
-//        int n1, n2, n3, n4, n5;
+       int n1, n2, n3, n4, n5;
 
-//        n1=int.Parse(Console.ReadLine());
-//        n2 = int.Parse(Console.ReadLine());
-//        n3 = int.Parse(Console.ReadLine());
-//        n4 = int.Parse(Console.ReadLine());
+       n1=int.Parse(Console.ReadLine());
+       n2 = int.Parse(Console.ReadLine());
+       n3 = int.Parse(Console.ReadLine());
+       n4 = int.Parse(Console.ReadLine());
 
-//        Console.WriteLine("find the average of these four number");
+       Console.WriteLine("find the average of these four number");
 
-//        Console.WriteLine("the average of the no is: "+(n1 + n2 + n3 + n4)/4);
+       Console.WriteLine("the average of the no is: "+(n1 + n2 + n3 + n4)/4);
 
-//    }
-//}
+   }
+}
 
