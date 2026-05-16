734ecf3bc3eefb363b177011b4515656812e7381
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-16 23:44:25 +0530

    Update Myprofile.cs

--- C_Sharp Codes/Myprofile.cs
+++ C_Sharp Codes/Myprofile.cs
@@ -1,54 +1,54 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 
-//   class Myprofile
-//    {
-//        public static void Main(string[] args)
-//        {
+  class Myprofile
+   {
+       public static void Main(string[] args)
+       {
 
-//        // direct print
-//        Console.WriteLine("***Myprofile***");
-//        Console.WriteLine( "Name:Rushikesh");
-//        Console.WriteLine("Email:rushi@gmail.com");
-//        Console.WriteLine("Age:23");
-//        Console.WriteLine("Address:pune");
-//        Console.WriteLine("Phone:3456789876");
+       // direct print
+       Console.WriteLine("***Myprofile***");
+       Console.WriteLine( "Name:Rushikesh");
+       Console.WriteLine("Email:rushi@gmail.com");
+       Console.WriteLine("Age:23");
+       Console.WriteLine("Address:pune");
+       Console.WriteLine("Phone:3456789876");
 
-//        Console.WriteLine();
-//        //user input
-//        Console.WriteLine("***Myprofile***");
+       Console.WriteLine();
+       //user input
+       Console.WriteLine("***Myprofile***");
 
-//        Console.Write("Name:");
-//        Console.ReadLine();
+       Console.Write("Name:");
+       Console.ReadLine();
 
-//        Console.Write("Email:");
-//        Console.ReadLine();
+       Console.Write("Email:");
+       Console.ReadLine();
 
-//        Console.Write("Age:");
-//        int.Parse(Console.ReadLine());
+       Console.Write("Age:");
+       int.Parse(Console.ReadLine());
 
-//        Console.Write("Address:");
-//        Console.ReadLine();
+       Console.Write("Address:");
+       Console.ReadLine();
 
-//        Console.Write("Phone:");
-//        long.Parse(Console.ReadLine());
+       Console.Write("Phone:");
+       long.Parse(Console.ReadLine());
         
-//        Console.Write("pincode");
-//        int.Parse(Console.ReadLine());
+       Console.Write("pincode");
+       int.Parse(Console.ReadLine());
         
-//        Console.Write("ssc marks");
-//        float.Parse(Console.ReadLine());
+       Console.Write("ssc marks");
+       float.Parse(Console.ReadLine());
 
-//        Console.Write("Hsc marks");
-//        float.Parse(Console.ReadLine());
-
-
+       Console.Write("Hsc marks");
+       float.Parse(Console.ReadLine());
 
 
 
-//    }
 
-//    }
+
+   }
+
+   }
 
