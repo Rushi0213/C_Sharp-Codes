463bc71dfbb47cf02cd78d09f46e66a1cdf48f04
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-16 23:43:57 +0530

    from user do while loop code in c#

---
+++ C_Sharp Codes/enterName_dowhileLoop.cs
@@ -0,0 +1,26 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+
+//     class enterName_dowhileLoop
+//    {
+
+//   public static void Main(string[] args)
+//    {
+//        Console.WriteLine("enter the name");
+//        int no =Convert.ToInt32(Console.ReadLine());
+//        string name = "rushi";
+//        int i = 1;
+//        do 
+//        { 
+        
+//            Console.WriteLine(i+" "+name);
+//            i++;
+            
+//        }
+//        while (i<no);
+        
+//    }
+//}
+
