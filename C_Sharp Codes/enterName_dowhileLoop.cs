3f491804a52953af303f428c3c696c7d562d364c
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-17 22:29:07 +0530

    Update enterName_dowhileLoop.cs

--- C_Sharp Codes/enterName_dowhileLoop.cs
+++ C_Sharp Codes/enterName_dowhileLoop.cs
@@ -1,26 +1,26 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 
-//     class enterName_dowhileLoop
-//    {
+    class enterName_dowhileLoop
+   {
 
-//   public static void Main(string[] args)
-//    {
-//        Console.WriteLine("enter the name");
-//        int no =Convert.ToInt32(Console.ReadLine());
-//        string name = "rushi";
-//        int i = 1;
-//        do 
-//        { 
+  public static void Main(string[] args)
+   {
+       Console.WriteLine("enter the name");
+       int no =Convert.ToInt32(Console.ReadLine());
+       string name = "rushi";
+       int i = 1;
+       do 
+       { 
         
-//            Console.WriteLine(i+" "+name);
-//            i++;
+           Console.WriteLine(i+" "+name);
+           i++;
             
-//        }
-//        while (i<no);
+       }
+       while (i<no);
         
-//    }
-//}
+   }
+}
 
