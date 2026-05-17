25e3a4150846e993dd133f0ee699bafa46da439a
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-17 22:27:52 +0530

    Update FacebookRegister.cs

--- C_Sharp Codes/FacebookRegister.cs
+++ C_Sharp Codes/FacebookRegister.cs
@@ -1,52 +1,52 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Reflection;
-//using System.Runtime.Intrinsics.Arm;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.Intrinsics.Arm;
+using System.Text;
 
 
-//    class FacebookRegister
-//    {
+   class FacebookRegister
+   {
      
-//    public static void Main(string[] args)
-//    {
-//        Console.WriteLine("****Facebook Registration Form****");
-//        Console.WriteLine();
+   public static void Main(string[] args)
+   {
+       Console.WriteLine("****Facebook Registration Form****");
+       Console.WriteLine();
 
-//        Console.WriteLine("Enter your Id*");
-//        int a=  int.Parse(Console.ReadLine());
-//        Console.WriteLine("my Id is:-"+a);
-//        Console.WriteLine();
+       Console.WriteLine("Enter your Id*");
+       int a=  int.Parse(Console.ReadLine());
+       Console.WriteLine("my Id is:-"+a);
+       Console.WriteLine();
 
-//        Console.WriteLine("Enter your name*");
-//        string b= Console.ReadLine();
-//        Console.WriteLine("my name is:-"+b);
+       Console.WriteLine("Enter your name*");
+       string b= Console.ReadLine();
+       Console.WriteLine("my name is:-"+b);
 
-//        Console.WriteLine();
+       Console.WriteLine();
 
-//        Console.WriteLine("enter your Email*");
-//      string c=Console.ReadLine();
-//        Console.WriteLine("my email is:-"+c);
+       Console.WriteLine("enter your Email*");
+     string c=Console.ReadLine();
+       Console.WriteLine("my email is:-"+c);
 
-//        Console.WriteLine();
+       Console.WriteLine();
 
 
-//        Console.WriteLine("Enter your phone no*");
-//       int d=int.Parse(Console.ReadLine());
-//        Console.WriteLine("my phone no is:-"+d);
+       Console.WriteLine("Enter your phone no*");
+      int d=int.Parse(Console.ReadLine());
+       Console.WriteLine("my phone no is:-"+d);
 
-//        Console.WriteLine();
+       Console.WriteLine();
 
-//        Console.WriteLine("enter your passwod*");
-//       string e= Console.ReadLine();
-//        Console.WriteLine("my password is:-"+e);
+       Console.WriteLine("enter your passwod*");
+      string e= Console.ReadLine();
+       Console.WriteLine("my password is:-"+e);
 
-//        Console.WriteLine();
+       Console.WriteLine();
 
-//        Console.WriteLine("REGISTER");
+       Console.WriteLine("REGISTER");
 
         
-//    }
+   }
 
-//    }
+   }
 
