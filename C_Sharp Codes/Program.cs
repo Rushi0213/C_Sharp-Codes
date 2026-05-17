4aafc77fd047a31639b3b15fcc170e4571b0debd
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-17 22:28:21 +0530

    Update Program.cs

--- C_Sharp Codes/Program.cs
+++ C_Sharp Codes/Program.cs
@@ -1,5 +1,5 @@
-﻿//Console.WriteLine("Hello, World!");
-//Console.WriteLine("1,2,3,4,5");
-//Console.WriteLine("_,/,.,>,<");
-//Console.WriteLine("@,#,$,%<^<&,*,!");
-//Console.WriteLine(2);
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("1,2,3,4,5");
+Console.WriteLine("_,/,.,>,<");
+Console.WriteLine("@,#,$,%<^<&,*,!");
+Console.WriteLine(2);
