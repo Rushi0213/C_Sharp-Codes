cc8010df0897d16d552b5631893f4ebcbf8b87c3
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-18 22:19:53 +0530

    one D array without declaring size

---
+++ C_Sharp Codes/one_D_array_withoutsize.cs
@@ -0,0 +1,17 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+
+//   class one_D_array_withoutsize
+//    {
+//    public static void Main(string[] args)
+//    {
+//        int[] arr = { 23, 34, 45, 46 };
+//        for(int i=0;i<arr.Length;i++)
+//        {
+//            Console.WriteLine("the array is: " + arr[i]);
+//        }
+//    }
+//}
+
