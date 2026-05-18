ddad6d2a8a21582f9551022b654d6bd67294a5e0
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-18 22:24:04 +0530

    print from user input index array of nth number

---
+++ C_Sharp Codes/print_userInput_nth_index_one_D_array2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Csharp_programing
+{
+    internal class print_userInput_nth_index_one_D_array2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the size od array");
+            int size =Convert.ToInt32(Console.ReadLine());
+            int[] arr=new int[size];
+
+            Console.WriteLine("enter the element");
+
+            for(int i=0;i<arr.Length;i++)
+            {
+                arr[i]=Convert.ToInt32(Console.ReadLine());
+
+            }
+
+            Console.WriteLine("enter the index");
+            int index=Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i == index)
+                {
+                    Console.WriteLine("element is: " + arr[i]);
+                }
+                else
+                {
+                    Console.WriteLine("index not found");
+                }
+            }
+        }
+    }
+}
