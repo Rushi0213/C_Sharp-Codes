7bfc03f51515a271a59ef79402cbcd48f02e8c14
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-18 22:24:32 +0530

    Update PrimeNo_forloop.cs

--- C_Sharp Codes/PrimeNo_forloop.cs
+++ C_Sharp Codes/PrimeNo_forloop.cs
@@ -1,25 +1,25 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 
-//     class PrimeNo_forloop
-//    {
-//   public static void Main(string[] args)
-//    {
-//        //prime no is divisible by itself and 1
+    class PrimeNo_forloop
+   {
+  public static void Main(string[] args)
+   {
+       //prime no is divisible by itself and 1
 
-//        //int no=Convert.ToInt32( Console.ReadLine());
-//        int no = 6;
+       //int no=Convert.ToInt32( Console.ReadLine());
+       int no = 6;
 
-//        for (int i = 2; i < no; i++)
-//        {
-//            if(no%2==0)
-//            {
-//                Console.WriteLine(" Not prime");
-//            }
+       for (int i = 2; i < no; i++)
+       {
+           if(no%2==0)
+           {
+               Console.WriteLine(" Not prime");
+           }
             
-//        }
-//    }
-//}
+       }
+   }
+}
 
