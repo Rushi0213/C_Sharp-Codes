0ee8e2d6b21b9bfe9723585181d00774216b0b80
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-19 23:20:17 +0530

    Update ifstmt.cs

--- C_Sharp Codes/ifstmt.cs
+++ C_Sharp Codes/ifstmt.cs
@@ -1,20 +1,20 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 
-//  class ifstmt
-//    {
+ class ifstmt
+   {
 
-//   public static void Main(string[] args)
-//    {
-//        //Console.WriteLine(" ");
-//        int No=100;
+  public static void Main(string[] args)
+   {
+       //Console.WriteLine(" ");
+       int No=100;
 
-//        if(No>0)
-//        {
-//            Console.WriteLine("no is possitive");
-//        }
-//    }
-//}
+       if(No>0)
+       {
+           Console.WriteLine("no is possitive");
+       }
+   }
+}
 
