8f71f2a38087de75c55c6bad31098bcfc3207d22
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-19 23:18:37 +0530

    Update Reverseno_ForLoop.cs

--- C_Sharp Codes/Reverseno_ForLoop.cs
+++ C_Sharp Codes/Reverseno_ForLoop.cs
@@ -1,23 +1,23 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 
-//   class Reverseno_ForLoop
-//    {
-//   public static void Main(string[] args)
-//    {
-//        Console.WriteLine("enter the  stating reverse number");
+  class Reverseno_ForLoop
+   {
+  public static void Main(string[] args)
+   {
+       Console.WriteLine("enter the  stating reverse number");
 
-//        int Sno=Convert.ToInt32(Console.ReadLine());
-//        Console.WriteLine("enter the  Ending reverse number");
-//        int Eno=Convert.ToInt32(Console.ReadLine());
-//        int i;
-//        for(i=Sno;i>=Eno;i--)
-//        {
-//            Console.WriteLine(i);
-//        }
-//    }
+       int Sno=Convert.ToInt32(Console.ReadLine());
+       Console.WriteLine("enter the  Ending reverse number");
+       int Eno=Convert.ToInt32(Console.ReadLine());
+       int i;
+       for(i=Sno;i>=Eno;i--)
+       {
+           Console.WriteLine(i);
+       }
+   }
 
-//}
+}
 
