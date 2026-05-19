5b45ae4955ab03ae40b8da28a7f77eab615b1d37
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-19 23:19:08 +0530

    Update one_D_array_withSize.cs

--- C_Sharp Codes/one_D_array_withSize.cs
+++ C_Sharp Codes/one_D_array_withSize.cs
@@ -1,25 +1,25 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Runtime.Intrinsics.Arm;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Intrinsics.Arm;
+using System.Text;
 
 
-//class one_D_array_withSize
-//{
-//    static void Main(string[] args)
-//    {
-//        int[] arr = new int[4];
-//        arr[0] = 1;
-//        arr[1] = 2;
-//        arr[2] = 3;
-//        arr[3] = 4;
+class one_D_array_withSize
+{
+   static void Main(string[] args)
+   {
+       int[] arr = new int[4];
+       arr[0] = 1;
+       arr[1] = 2;
+       arr[2] = 3;
+       arr[3] = 4;
 
-//        int index = 0;
-//        while (index < arr.Length)
-//        {
-//            Console.WriteLine("the array is " + arr[index]);
-//            index++;
-//        }
-//    }
-//}
+       int index = 0;
+       while (index < arr.Length)
+       {
+           Console.WriteLine("the array is " + arr[index]);
+           index++;
+       }
+   }
+}
 
