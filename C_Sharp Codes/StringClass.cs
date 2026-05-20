9a1005265948689229527a6ddd759fec538c89d0
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-20 23:58:32 +0530

    this is three way of string use in c#

---
+++ C_Sharp Codes/StringClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+    class StringClass
+    {
+    static void Main(string[] args)
+    {
+        //1) simple string
+        string newname = "rushikesh";
+        Console.WriteLine("string:"+newname);
+
+
+        //2) string class
+
+        string city = new string("nashik");
+        Console.WriteLine(" before city:"+city);
+        Console.WriteLine("uppercase:"+city.ToUpper());
+        Console.WriteLine("after city:"+city);
+
+        Console.WriteLine("--------------------------------------------");
+        
+
+        //3) stringbuilder class
+
+        StringBuilder name = new StringBuilder("Rushikesh");
+        Console.WriteLine("before:"+name);
+        Console.WriteLine("append:"+name.Append(" Deshmukh"));
+        Console.WriteLine("after:"+name);
+        Console.WriteLine("length:"+name.Length);
+        Console.WriteLine("remove:"+name.Remove(10,8));
+       
+    }
+}
+
