37cf9212503c992155f572ad6d07923a1d09af81
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-20 23:59:25 +0530

    Update StringMethods.cs

--- C_Sharp Codes/StringMethods.cs
+++ C_Sharp Codes/StringMethods.cs
@@ -1,28 +1,28 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 
-//     class StringMethods
-//    {
-//    static void Main(string[] args)
-//    {
-//        string name = " Rushikesh ";
-//        Console.WriteLine("string is:"+name);
-//        Console.WriteLine("Touppercse:"+name.ToUpper());
-//        Console.WriteLine("tolower:"+name.ToLower());
-//        Console.WriteLine("replace:"+name.Replace('h','m'));
-//        Console.WriteLine("trim:"+name.Trim());
-//        Console.WriteLine("length:"+name.Length);
-//        Console.WriteLine("substrin:"+name.Substring(5));
-//        Console.WriteLine("index of charecter: "+name.IndexOf("k"));
-//        Console.WriteLine("contains:"+name.Contains("i"));
-//        Console.WriteLine("append:"+name.Append("is my name"));
-//        char[] arr = name.ToArray();
-//        for(int i=0;i<arr.Length;i++)
-//        {
-//            Console.WriteLine(arr[i]);
-//        }
-//    }
-//}
+    class StringMethods
+   {
+   static void Main(string[] args)
+   {
+       string name = " Rushikesh ";
+       Console.WriteLine("string is:"+name);
+       Console.WriteLine("Touppercse:"+name.ToUpper());
+       Console.WriteLine("tolower:"+name.ToLower());
+       Console.WriteLine("replace:"+name.Replace('h','m'));
+       Console.WriteLine("trim:"+name.Trim());
+       Console.WriteLine("length:"+name.Length);
+       Console.WriteLine("substrin:"+name.Substring(5));
+       Console.WriteLine("index of charecter: "+name.IndexOf("k"));
+       Console.WriteLine("contains:"+name.Contains("i"));
+       Console.WriteLine("append:"+name.Append("is my name"));
+       char[] arr = name.ToArray();
+       for(int i=0;i<arr.Length;i++)
+       {
+           Console.WriteLine(arr[i]);
+       }
+   }
+}
 
