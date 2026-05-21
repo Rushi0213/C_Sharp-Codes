b2cfda5333eebf6a814c787e3cab84d45563aff8
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-21 23:08:32 +0530

    fibonacci series using user input

---
+++ C_Sharp Codes/Fibonacii_Series_using_non_Static_method.cs
@@ -0,0 +1,41 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Runtime.Intrinsics.Arm;
+//using System.Text;
+
+
+//class Fibonacii_Series_using_non_Static_method
+//{
+
+//    void fibonaci(int num,int a, int b)
+//    {
+//        int c;
+
+//        for (int i = 0; i < num; i++)
+//        {
+//            Console.WriteLine(a);
+//            c = a + b;
+//            a = b;
+//            b = c;
+
+
+//        }
+//    }
+//    public static void Main(string[] args)
+//    {
+//        Fibonacii_Series_using_non_Static_method sc = new Fibonacii_Series_using_non_Static_method();
+
+//        Console.WriteLine("enter limit");
+//        int num = Convert.ToInt32(Console.ReadLine());
+
+//        Console.WriteLine("enter the vslue of a");
+//        int m = Convert.ToInt32(Console.ReadLine());
+
+//        Console.WriteLine("enter the the value of b");
+//        int n = Convert.ToInt32(Console.ReadLine());
+
+//        sc.fibonaci(num,m,n);
+
+//    }
+//}
+
