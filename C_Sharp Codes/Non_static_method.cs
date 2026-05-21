5ba99491e4b4a0795dbfb75ce6fcd4a83a626399
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-21 23:16:55 +0530

    non static method second code in c#

---
+++ C_Sharp Codes/Non_static_method.cs
@@ -0,0 +1,33 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+
+//  class Non_static_method
+//    {
+//    void Name()//non static method
+//    {
+//        Console.WriteLine("my name is rushikesh");
+//    }
+//    int phno(int n)// return type method
+//    {
+//        return n;
+//    }
+//    void City(string city)//parameterized non static method
+//    {
+//        Console.WriteLine("my city is:"+city);
+//    }
+//    static void Main(string[] args)
+//    {
+//        Non_static_method sc= new Non_static_method();
+       
+//        sc.Name();//default method
+
+//        Console.WriteLine("phone no is:"+sc.phno(6788779));// this parameterized using return type
+        
+//        sc.City("Nahik");// this is parameterized method
+//    }
+
+//}
+
+
