e315aee9c36f705fe36c598d4102cc2b305fc3ab
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-21 23:15:28 +0530

    without inbuild method contain print the present

---
+++ C_Sharp Codes/ContainStringWithout_Inbuilt.cs
@@ -0,0 +1,32 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+
+//class ContainStringWithout_Inbuilt
+//{
+//    static void Main(string[] args)
+//    {
+//        Console.WriteLine("Enter a string:");
+//        string str = Console.ReadLine();
+
+//        Console.WriteLine("Enter a character:");
+//        char ch = Convert.ToChar(Console.ReadLine());
+
+//        int flag = 0;
+
+//        for (int i = 0; i < str.Length; i++)
+//        {
+//            if (str[i] == ch)
+//            {
+//                flag = 1;
+//                break;
+//            }
+//        }
+
+//        if (flag == 1)
+//            Console.WriteLine("Character is found");
+//        else
+//            Console.WriteLine("Character is not found");
+//    }
+//}
