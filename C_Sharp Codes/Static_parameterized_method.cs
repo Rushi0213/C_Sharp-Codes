6aa8a20ec2bb8585483461783849051776ea1294
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-21 23:12:26 +0530

    static parameterized method in c#

---
+++ C_Sharp Codes/Static_parameterized_method.cs
@@ -0,0 +1,23 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+
+//    class Static_parameterized_method
+//    {
+
+//    static void Name(string name)
+//    {
+//        Console.WriteLine(" my name is: "+name);
+//    }
+//    static void Number(int number)
+//    {
+//        Console.WriteLine("my number is: "+number);
+//    }
+//    static void Main(string[] args)
+//    {
+//        Name("Rushikesh");
+//        Number(1234567890);
+//    }
+//}
+
