fa9be72c63b1f55ee4660f7509e20dc7c1f40c5b
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-21 23:10:21 +0530

    non static method in c#

---
+++ C_Sharp Codes/Static_Default_Method.cs
@@ -0,0 +1,33 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+
+//   class Static_Default_Method
+//    {
+
+//    static void name()// without using return type static method
+//                      // when we use void the no need to use return type
+//    {
+//        Console.WriteLine("my name is rushikesh");
+//    }
+
+//    static int num(int a)// using return type static method
+//        //when we use  int,string,bool datatype then their is need to use return type
+//    {
+//        return a;
+//    }
+//    static string city()//here used string datatype and return it and call it in main method
+//    {
+//        return "Nashik";
+//    }
+//   public static void Main(string[] args)
+//    {
+//       name();//call without using class name
+//        Static_Default_Method.name();//call using class name
+//        Console.WriteLine(num(10));//call without using class name
+//        Console.WriteLine(Static_Default_Method.num(20));//call using class name
+//        Console.WriteLine(city());
+//    }
+//}
+
