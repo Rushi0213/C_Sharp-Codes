ff0a3c4c964bfcd91c78d048a6ba2bbe610b302b
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-21 23:06:59 +0530

    find average using parameterized method in c#

---
+++ C_Sharp Codes/Average_using_para_method.cs
@@ -0,0 +1,29 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Text;
+
+
+//   class Average_using_para_method
+//    {
+
+//    void average(int n1 ,int n2,int n3,int n4)
+//    {
+//        int avg = ((n1 + n2 + n3 + n4) / 4);
+         
+//        Console.WriteLine("average is :"+avg);
+//    }
+//    static void Main(string[] args) 
+//    {
+//        Average_using_para_method sc=new Average_using_para_method();
+
+//        Console.WriteLine("enter the user number to find the average");
+//        int a=Convert.ToInt32(Console.ReadLine());
+//        int b=Convert.ToInt32(Console.ReadLine());
+//        int c=Convert.ToInt32(Console.ReadLine());
+//        int d=Convert.ToInt32(Console.ReadLine());
+//        sc.average(a,b,c,d);
+//    }
+
+//}
+
