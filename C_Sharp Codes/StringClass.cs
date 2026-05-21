b7c8daeaf5d2b38d7da28124154fb14c96801f0e
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-21 23:13:28 +0530

    string in c#

--- C_Sharp Codes/StringClass.cs
+++ C_Sharp Codes/StringClass.cs
@@ -1,36 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
 
-    class StringClass
-    {
-    static void Main(string[] args)
-    {
-        //1) simple string
-        string newname = "rushikesh";
-        Console.WriteLine("string:"+newname);
+//    class StringClass
+//    {
+//    static void Main(string[] args)
+//    {
+//        //1) simple string
+//        string newname = "rushikesh";
+//        Console.WriteLine("string:"+newname);
 
 
-        //2) string class
+//        //2) string class
 
-        string city = new string("nashik");
-        Console.WriteLine(" before city:"+city);
-        Console.WriteLine("uppercase:"+city.ToUpper());
-        Console.WriteLine("after city:"+city);
+//        string city = new string("nashik");
+//        Console.WriteLine(" before city:"+city);
+//        Console.WriteLine("uppercase:"+city.ToUpper());
+//        Console.WriteLine("after city:"+city);
 
-        Console.WriteLine("--------------------------------------------");
+//        Console.WriteLine("--------------------------------------------");
         
 
-        //3) stringbuilder class
+//        //3) stringbuilder class
 
-        StringBuilder name = new StringBuilder("Rushikesh");
-        Console.WriteLine("before:"+name);
-        Console.WriteLine("append:"+name.Append(" Deshmukh"));
-        Console.WriteLine("after:"+name);
-        Console.WriteLine("length:"+name.Length);
-        Console.WriteLine("remove:"+name.Remove(10,8));
+//        StringBuilder name = new StringBuilder("Rushikesh");
+//        Console.WriteLine("before:"+name);
+//        Console.WriteLine("append:"+name.Append(" Deshmukh"));
+//        Console.WriteLine("after:"+name);
+//        Console.WriteLine("length:"+name.Length);
+//        Console.WriteLine("remove:"+name.Remove(10,8));
        
-    }
-}
+//    }
+//}
 
