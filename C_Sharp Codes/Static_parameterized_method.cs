258254646db7ffaffa9ceac5227feac458ab90d6
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-24 22:49:45 +0530

    Update Static_parameterized_method.cs

--- C_Sharp Codes/Static_parameterized_method.cs
+++ C_Sharp Codes/Static_parameterized_method.cs
@@ -1,23 +1,23 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 
-//    class Static_parameterized_method
-//    {
+   class Static_parameterized_method
+   {
 
-//    static void Name(string name)
-//    {
-//        Console.WriteLine(" my name is: "+name);
-//    }
-//    static void Number(int number)
-//    {
-//        Console.WriteLine("my number is: "+number);
-//    }
-//    static void Main(string[] args)
-//    {
-//        Name("Rushikesh");
-//        Number(1234567890);
-//    }
-//}
+   static void Name(string name)
+   {
+       Console.WriteLine(" my name is: "+name);
+   }
+   static void Number(int number)
+   {
+       Console.WriteLine("my number is: "+number);
+   }
+   static void Main(string[] args)
+   {
+       Name("Rushikesh");
+       Number(1234567890);
+   }
+}
 
