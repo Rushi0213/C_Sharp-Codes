dafd4ed9f85111535c07408465584d5294c672af
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-24 22:42:33 +0530

    call by value  code n c#

---
+++ C_Sharp Codes/callByValue.cs
@@ -0,0 +1,23 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace Csharp_programing
+//{
+//    internal class callByValue
+//    {
+
+//        static void call(int x)
+//        {
+//            x++;
+//            Console.WriteLine("value is"+x);
+//        }
+//        static void Main(string[] args)
+//        {
+//            int a = 10;
+//            Console.WriteLine("before passing value"+a);
+//            call(a);
+//            Console.WriteLine("after passing value"+a);
+//        }
+//    }
+//}
