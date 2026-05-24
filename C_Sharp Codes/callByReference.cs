48c46f5f56dc22b6ccaf147afd8b1a881715dad1
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-24 22:43:05 +0530

    call by refrence in c#

---
+++ C_Sharp Codes/callByReference.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Csharp_programing
+{
+    internal class callByReference
+    {
+        static void referece( ref int x)
+        {
+            x++;
+            Console.WriteLine("value is:"+x);
+        }
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Console.WriteLine("before passing value:"+a);
+            referece( ref a);
+            Console.WriteLine("after passing value:"+a);
+        }
+    }
+}
