6cefe52de766ad2f9642630c3ddd8dc08a80da48
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-25 23:09:50 +0530

    class and object in c#

---
+++ C_Sharp Codes/class_object.cs
@@ -0,0 +1,34 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+
+//   class class_object
+//    {
+//    string name;//gobal variable or instanceand properties of class
+//    int height;
+//    void Walk()// behaviour of class or method
+//    {
+//        Console.WriteLine(name+" is walking");
+//    }
+//    void Read()
+//    {
+//        Console.WriteLine(name +"  is reading");
+//    }
+//    static void Main(string[] args)
+//    {
+//        class_object sc= new class_object();// real world entity  or object
+
+//        //object access propertie ,behaviours of stuent class
+//        //name
+//        sc.name = "Rushikesh";
+//        Console.WriteLine("name is: "+sc.name);
+//        sc.Walk();
+//        sc.Read();
+
+//        //height
+//        sc.height = 143;
+//        Console.WriteLine("height is: "+sc.height);
+//    }
+//}
+
