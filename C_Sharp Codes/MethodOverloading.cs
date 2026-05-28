2db2f0b92a5bd8185095ff8a6e6de35fb67cd5f3
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-28 23:27:12 +0530

    method overloading in  polymorphism c#

---
+++ C_Sharp Codes/MethodOverloading.cs
@@ -0,0 +1,40 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+
+//    class MethodOverloading
+//    {
+//      void billPayment()
+//    {
+//        Console.WriteLine("bill payment of electricity bill");
+//    }
+//    void billPayment(int amount)
+//    {
+//        Console.WriteLine("bill payment of waterbill");
+//    }
+//    void billPayment(string amount)
+//    {
+//        Console.WriteLine("bill payment of telephone bill");
+//    }
+//    void billPayment(int amount, string name)
+//    {
+//        Console.WriteLine("bill payment of credit card bill :"+" amount of bill is : "+amount+"  name of bill payer :"+name);
+//    }
+
+//    void billPayment(string name,int amount)
+//    {
+//        Console.WriteLine("bill payment of credit card bill :" + " amount of bill is : " + amount + "  name of bill payer :" + name);
+//    }
+
+//    static void Main(string[] args)
+//    {
+//        MethodOverloading m= new MethodOverloading();
+//        m.billPayment();
+//        m.billPayment(2000);
+//        m.billPayment("20000");
+//        m.billPayment(1000,"rushikesh");
+//        m.billPayment("rushikesh",15000);
+//    }
+//}
+
