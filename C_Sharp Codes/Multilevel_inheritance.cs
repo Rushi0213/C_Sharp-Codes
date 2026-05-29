91310f5937efab8874efc9bbdf33a9d3dd780f4e
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-29 23:12:10 +0530

    multilevel inheritance

---
+++ C_Sharp Codes/Multilevel_inheritance.cs
@@ -0,0 +1,39 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+
+//   class Multilevel_inheritance
+//    {
+    
+//   public static void grnadpa()
+//    {
+//        Console.WriteLine("this is grnafather class");
+//    }
+
+//}
+//class parent:Multilevel_inheritance
+//{
+//  public  void parents()
+//    {
+//        Console.WriteLine("this is parent class");
+//    }
+//}
+//class child:parent
+//{
+//   public void childs()
+//    {
+//        Console.WriteLine("this is child class");
+//    }
+//}
+//class main()
+//{
+//    static void Main(string[] args)
+//    {
+//        child c = new child();
+//        c.parents();
+//        c.childs();
+
+//        Multilevel_inheritance.grnadpa();
+//    }
+//}
