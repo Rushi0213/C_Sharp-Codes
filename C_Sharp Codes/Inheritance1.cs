c02a2da072ba11505f14ecf09fc189bbbc502441
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-29 23:09:44 +0530

    inhiritance file first

---
+++ C_Sharp Codes/Inheritance1.cs
@@ -0,0 +1,30 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Runtime.Intrinsics.Arm;
+//using System.Text;
+
+
+//    class Inheritance1
+//    {
+//        public void Home()
+//        {
+//            Console.WriteLine("this is parent home");
+//        }
+
+//    }
+//class child:Inheritance1
+//{
+//    public void main()
+//    {
+//        Console.WriteLine("this is child");
+//    }
+
+//    static void Main(string[] args)
+//    {
+//        child sc= new child();
+//        sc.main();
+//        sc.Home();
+//    }
+//}
+
+
