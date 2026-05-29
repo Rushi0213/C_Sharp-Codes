fce0f14025eabe6566906ec3d4b4fed125312995
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-29 23:11:27 +0530

    herarchical inheritance

---
+++ C_Sharp Codes/herarchical_inheritance.cs
@@ -0,0 +1,48 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Runtime.Intrinsics.Arm;
+//using System.Text;
+//using System.Threading.Channels;
+
+
+//class herarchical_inheritance
+//{
+
+
+// public  void parent()
+//    {
+//        Console.WriteLine("this is parent");
+//    }
+
+
+
+//}
+//class child1:herarchical_inheritance
+//{
+//   public void childs1()
+//    {
+//        Console.WriteLine("this is child1 class");
+//    }
+//}
+//class child2:herarchical_inheritance
+//{
+//  public  void chilrds2()
+//    {
+//        Console.WriteLine("this is child 2 class");
+//    }
+//}
+
+//class Mains()
+//{
+//    static void Main(string[] args)
+//    {
+//        child1 c1 = new child1();
+//        child2 c2 = new child2();
+//        c1.parent();
+//        c1.childs1();
+
+//        c2.parent();
+//        c2.chilrds2();
+
+//    }
+//}
