291ddf34e88dd5b58ec12629c3ce62ae7ccdec7b
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-29 23:23:10 +0530

    Update Inheritance1.cs

--- C_Sharp Codes/Inheritance1.cs
+++ C_Sharp Codes/Inheritance1.cs
@@ -1,30 +1,30 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Runtime.Intrinsics.Arm;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Intrinsics.Arm;
+using System.Text;
 
 
-//    class Inheritance1
-//    {
-//        public void Home()
-//        {
-//            Console.WriteLine("this is parent home");
-//        }
+   class Inheritance1
+   {
+       public void Home()
+       {
+           Console.WriteLine("this is parent home");
+       }
 
-//    }
-//class child:Inheritance1
-//{
-//    public void main()
-//    {
-//        Console.WriteLine("this is child");
-//    }
+   }
+class child:Inheritance1
+{
+   public void main()
+   {
+       Console.WriteLine("this is child");
+   }
 
-//    static void Main(string[] args)
-//    {
-//        child sc= new child();
-//        sc.main();
-//        sc.Home();
-//    }
-//}
+   static void Main(string[] args)
+   {
+       child sc= new child();
+       sc.main();
+       sc.Home();
+   }
+}
 
 
