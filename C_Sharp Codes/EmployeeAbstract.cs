5e285b59411c3e759d171124a7c073d3102d4f69
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-29 23:22:21 +0530

    Update EmployeeAbstract.cs

--- C_Sharp Codes/EmployeeAbstract.cs
+++ C_Sharp Codes/EmployeeAbstract.cs
@@ -1,35 +1,35 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 
-//    abstract class EmployeeAbstract
-//    {
-//    public abstract void employeee();
-//    public void salary()
-//    {
-//        Console.WriteLine("the salary is 50000");
-//    }
-//}
+   abstract class EmployeeAbstract
+   {
+   public abstract void employeee();
+   public void salary()
+   {
+       Console.WriteLine("the salary is 50000");
+   }
+}
 
-// class employee:EmployeeAbstract
-//{
-//    public override void employeee()
-//    {
-//        Console.WriteLine("employeee id is 45678");
-//    }
+class employee:EmployeeAbstract
+{
+   public override void employeee()
+   {
+       Console.WriteLine("employeee id is 45678");
+   }
 
-//    //public void salary()
-//    //{
-//    //    Console.WriteLine("employee salary is 50000");
-//    //}
-//}
-//class main
-//{
-//    static void Main(string[] args)
-//    {
-//        employee e= new employee();
-//        e.employeee();
-//    }
-//}
+   //public void salary()
+   //{
+   //    Console.WriteLine("employee salary is 50000");
+   //}
+}
+class main
+{
+   static void Main(string[] args)
+   {
+       employee e= new employee();
+       e.employeee();
+   }
+}
 
