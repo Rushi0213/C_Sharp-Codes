ec6209d056b56fee0d7f75714a793306345bf70d
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-29 23:21:33 +0530

    Update BankInterface.cs

--- C_Sharp Codes/BankInterface.cs
+++ C_Sharp Codes/BankInterface.cs
@@ -1,49 +1,49 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 
-//interface BankInterface
-//{
-//    public void bankbalance();
+interface BankInterface
+{
+   public void bankbalance();
 
 
-//    public void bankloan();
+   public void bankloan();
 
-//    public void bankaccount();
-//    public void bankaccountloan()
-//    {
-//        Console.WriteLine("the loan on bank account loan is 234567");
-//    }
+   public void bankaccount();
+   public void bankaccountloan()
+   {
+       Console.WriteLine("the loan on bank account loan is 234567");
+   }
 
-//}
+}
 
 
-//class bankinfo : BankInterface
-//{
+class bankinfo : BankInterface
+{
 
-//    public void bankbalance()
-//    {
+   public void bankbalance()
+   {
 
-//        Console.WriteLine("bankbalance is 60 rs");
-//    }
-//    public void bankloan()
-//    {
-//        Console.WriteLine("bankloan is  5000rs");
-//    }
+       Console.WriteLine("bankbalance is 60 rs");
+   }
+   public void bankloan()
+   {
+       Console.WriteLine("bankloan is  5000rs");
+   }
 
-//    public void bankaccount()
-//    {
-//        Console.WriteLine("accountnumber is 1234567");
-//    }
+   public void bankaccount()
+   {
+       Console.WriteLine("accountnumber is 1234567");
+   }
 
-//    static void Main(string[] args)
-//    {
-//        bankinfo b = new bankinfo();
-//        b.bankbalance();
-//        b.bankloan();
-//        b.bankaccount();
-//        //b.bankaccountloan();
-//    }
-//}
+   static void Main(string[] args)
+   {
+       bankinfo b = new bankinfo();
+       b.bankbalance();
+       b.bankloan();
+       b.bankaccount();
+       //b.bankaccountloan();
+   }
+}
 
