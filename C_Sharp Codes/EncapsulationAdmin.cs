b1ef40e4a156153d0e411e5f4371da8ada1193b8
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-31 22:01:13 +0530

    encapsulation in c#

---
+++ C_Sharp Codes/EncapsulationAdmin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+     class EncapsulationAdmin
+    {
+
+    private int adminid;
+        private string adminpassword;
+
+
+    public void getadminidAndpassword(int adminid,string adminpasword)
+    {
+        this.adminid = adminid;//store int parameter in instance variable
+        this.adminpassword = adminpasword;//store string parameter in instance variable
+    }
+    public int setadminid()// this is adminid return method
+    {
+        return adminid;
+    }
+    public string setadminpassword()//this is adminpassword return method
+    {
+        return adminpassword;
+    }
+    
+    static void Main(string[] args)
+    {
+        EncapsulationAdmin a = new EncapsulationAdmin();
+        a.getadminidAndpassword(102,"admin@123");//calling parameterise method
+
+        Console.WriteLine("adminid is: "+a.setadminid());//calling return type adminid method
+        Console.WriteLine("adminpassword is:"+a.setadminpassword());//calling return type adminpassword method
+
+    }
+}
+
