c04d6be4bb72e53f322baacecf95eb9f3f4fa7ca
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-31 22:04:40 +0530

    this keyword in c#

---
+++ C_Sharp Codes/ThisKeyword.cs
@@ -0,0 +1,26 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+
+//     class ThisKeyword
+//    {
+
+//    int num;
+//    void displayvalue(int num)
+//    {
+//        this.num = num;//use this keyword when variable of instace and parameter variable are same
+//        Console.WriteLine("inside method");
+//        Console.WriteLine("number is: "+num);
+//    }
+//    static void Main(string[] args)
+//    {
+//        ThisKeyword t=new ThisKeyword();
+//        t.displayvalue(100);
+//        Console.WriteLine("using object access this value");
+//        Console.WriteLine("number is:"+t.num);
+//    }
+
+//}
+
+
