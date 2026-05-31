3c714055dfdc6db4780bfbc17ded00f37abb1e66
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-05-31 22:03:30 +0530

    multiple  inheritance using interface

---
+++ C_Sharp Codes/multiple_inheritance_interface.cs
@@ -0,0 +1,48 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+
+////multiple inheritance
+
+//interface Animal//interface class
+//{
+//    void Eat();
+//}
+//interface Pet//interface class
+//{
+//    void Play();
+//}
+
+//class Dog : Animal, Pet// inherite interface class
+//{
+//    public void Bark()// simple method
+//    {
+//        Console.WriteLine("dog is barking");
+//    }
+//    public void Eat()//animal interface method
+//    {
+//        Console.WriteLine("dog is eating");
+//    }
+//    public void Play()//pet interface method
+//    {
+//        Console.WriteLine("dog is playing");
+//    }
+
+//}
+
+//class multiple_inheritance_interface
+//{
+
+//    static void Main(string[] args)
+//    {
+//        Dog d = new Dog();//created object of chid class of both interface
+//        d.Bark();
+//        d.Eat();
+//        d.Play();
+//    }
+
+
+
+//}
+
