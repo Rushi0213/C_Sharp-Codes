ba9b2e514ade866460e925d833fe3ae11a84b831
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-06-01 21:07:00 +0530

    one try catch block with multiple  exception

---
+++ C_Sharp Codes/Multiple_tyr_catch.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+    class Multiple_tyr_catch
+    {
+    static int c;
+    static void Main(string[] args)
+    {
+        int a = 10;
+        int b = 0;
+        
+
+        string name = null;
+
+        try
+        {
+
+            int count=name.Length;//nullreference exception
+            c = a / b;//dividebyzeroexception
+  
+        }
+
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+
+    //    try
+    //    {
+    //        Console.WriteLine("name is :"+name);
+    //    }
+    //    catch(Exception f)
+    //    {
+    //        Console.WriteLine(f);
+    //    }
+    //}
+}
+
