eb09690f47f91cc0a70dad2a2a93338e69f9a0e4
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-06-01 21:08:48 +0530

    try catch block exception first code

---
+++ C_Sharp Codes/try_catch_exception.cs
@@ -0,0 +1,38 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+
+//class try_catch_exception
+//{
+
+
+//    static int c;
+//    static void Main(string[] args)
+//    {
+//        int a = 50;
+//        int b = 0;
+
+
+
+//        try
+//        {
+//            int c = a / b;
+//        }
+//        catch (Exception e)
+//        {
+//            Console.WriteLine("error  handled"+e);
+//        }
+
+//        Console.WriteLine();
+//        Console.WriteLine("output is :" + c);
+
+
+//    }
+//}
+
+
+
+    
+    
+
