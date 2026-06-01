e93a5c2a4a46c4133487b719b8e0b36ca5dd8a45
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-06-01 21:07:53 +0530

    try catch block one exception second code

---
+++ C_Sharp Codes/try_catch2.cs
@@ -0,0 +1,25 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+
+//    class try_catch2
+//    {
+//    static void Main(string[] args)
+//    {
+//        string name = null;
+
+//        Console.WriteLine("string is :"+name);
+
+//        try
+//        {
+//            Console.WriteLine("string length is:" + name.Length);
+//        }
+//        catch (Exception e)
+//        {
+//            Console.WriteLine("exception handled"+e);
+//        }
+        
+//    }
+//}
+
