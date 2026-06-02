c39803a1792af522594fbbd298eaa9fcbfd06293
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-06-02 23:01:19 +0530

    multiple try catch block

---
+++ C_Sharp Codes/Multiple_try_catch-exception.cs
@@ -0,0 +1,36 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+
+//class Multiple_try_catch_exception
+//{
+//    static void Main(string[] args)
+//    {
+//        int a = 10; 
+//        int b = 0;
+//        int c;
+
+//        string name = null;
+
+
+//        try
+//        {
+           
+//            int count = name.Length;
+//            c = a / b;
+
+//        }
+
+//        catch (DivideByZeroException e)
+//        {
+//            Console.WriteLine("divide byzero is not allowed");
+//        }
+
+//        catch (Exception ex)
+//        {
+//            Console.WriteLine("null refrence exception");
+//        }
+//    }
+//}
+
