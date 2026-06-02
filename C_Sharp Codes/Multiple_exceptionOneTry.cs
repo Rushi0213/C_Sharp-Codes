adeec51427c7c54c3e63bb2a17e871fd7461cdd7
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-06-02 23:00:29 +0530

    multiple exception one try block

---
+++ C_Sharp Codes/Multiple_exceptionOneTry.cs
@@ -0,0 +1,33 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+
+//    class Multiple_exceptionOneTry
+//    {
+//    static int c;
+//    static void Main(string[] args)
+//    {
+//        int a = 10;
+//        int b = 0;
+        
+
+//        string name = null;
+
+//        try
+//        {
+
+//            int count=name.Length;//nullreference exception
+//            c = a / b;//dividebyzeroexception
+  
+//        }
+//            //parent class of all exception:- e is refrence variable
+//        catch (Exception e)
+//        {
+//            Console.WriteLine(e);
+//        }
+//    }
+
+   
+//}
+
