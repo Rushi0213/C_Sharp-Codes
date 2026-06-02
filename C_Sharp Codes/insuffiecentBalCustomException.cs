a6a27bc08c098253e2a697d106a813dfa7f73748
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-06-02 22:59:43 +0530

    insufficent balance  custom validation code

---
+++ C_Sharp Codes/insuffiecentBalCustomException.cs
@@ -0,0 +1,35 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+
+
+//class InsufficentBalance:Exception
+//{
+//    public InsufficentBalance(string msg):base(msg)
+//    {
+
+//    }
+//}
+//    class insuffiecentBalCustomException
+//    {
+
+//    static void Main(string[] args)
+//    {
+//        Console.WriteLine("enter the balance");
+//        int Wbalance=Convert.ToInt32(Console.ReadLine());
+
+//        int balace = 10000;
+
+//        if(Wbalance<=balace)
+//        {
+//            Console.WriteLine("you can withdrwal money");
+//        }
+
+//        else
+//        {
+//           throw new InsufficentBalance("insufficent balace ");
+//        }
+//    }
+//}
+
