dfbd0e6c1520034b0a36fc775d361fc27ef6df7d
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-06-02 23:02:46 +0530

    multiple ways of try catch block

---
+++ C_Sharp Codes/ways_tryCatch_finally.cs
@@ -0,0 +1,59 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//  class ways_tryCatch_finally
+//    {
+
+//    static void Main(string[] args)
+//    {
+//        int a = 10;
+//        int b = 0;
+//        int c;
+
+//        string n = null;
+
+//        //try
+//        //{
+//        //    
+
+//        //    try
+//        //    {
+//        //        int count = n.Length;
+//        //    }
+//        //    finally
+//        //    {
+//        //        Console.WriteLine("executed");
+//        //    }
+//       // c = a / b;
+
+//        //}
+//        //catch(Exception e)
+//        //{
+//        //    Console.WriteLine(e);
+//        //}
+
+
+
+//        try
+//        {
+            
+//            try
+//            {
+//                int count = n.Length;
+//            }
+//            catch (NullReferenceException e)
+//            {
+//                Console.WriteLine(e);
+//            }
+//            c = a / b;
+//        }
+//        catch (DivideByZeroException m)
+//        {
+//            Console.WriteLine(m);
+//        }
+
+
+//    }
+//}
+
