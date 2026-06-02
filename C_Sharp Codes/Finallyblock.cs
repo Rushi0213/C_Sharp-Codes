b5234cf476d89fd95b68db16f5020564fc36f6d9
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-06-02 22:58:44 +0530

    try catch using finally keyword

---
+++ C_Sharp Codes/Finallyblock.cs
@@ -0,0 +1,31 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+
+//     class Finallyblock
+//    {
+//    static void Main(string[] args)
+//    {
+//        int a = 10;
+//        int b = 0;
+//        int c;
+//        string s = null;
+
+//        try
+//        {
+//            int count = s.Length;
+//            c = a / b;
+           
+//        }
+//        //catch (Exception e)
+//        //{
+//        //    Console.WriteLine(e);
+//        //}
+//        finally
+//        {
+//            Console.WriteLine("finally block is executed");
+//        }
+//    }
+//}
+
