c587ab3df02df703e5843ca8861a89a783849d09
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-06-02 22:57:59 +0530

    custom exception using throw keyword

---
+++ C_Sharp Codes/CustomException.cs
@@ -0,0 +1,31 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+
+
+//class EligibleException:Exception
+//{
+//    public EligibleException(string msg):base(msg)
+//    {
+
+//    }
+//}
+//     class CustomException
+//    {
+//    static void Main(string[] args)
+//    {
+//        Console.WriteLine("enter your age");
+//        int age=Convert.ToInt32(Console.ReadLine());
+
+//        if(age>=18)
+//        {
+//            Console.WriteLine("you are eligible for vote");
+//        }
+//        else
+//        {
+//           throw new EligibleException("you are not eligible for vote");
+//        }
+//    }
+//}
+
