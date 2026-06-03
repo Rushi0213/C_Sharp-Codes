725635f99f2103efbec6ffc63618a0ac129d155e
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-06-03 23:48:55 +0530

    file reading

---
+++ C_Sharp Codes/FileRead_FH.cs
@@ -0,0 +1,28 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+
+//class FileRead_FH
+//{
+//    void read()
+//    {
+//        string path = "D:\\VisualStudio\\Csharp\\Csharp_programing\\Csharp_programing\\newfile_FH.txt";
+//        FileStream fs = new FileStream(path, FileMode.OpenOrCreate);
+
+//        StreamReader sr = new StreamReader(fs);
+//        string alldata = sr.ReadToEnd();
+//        Console.WriteLine("data read done !");
+//        Console.WriteLine("the data is :");
+//        Console.WriteLine(alldata);
+//        sr.Close();
+//        fs.Close();
+
+//    }
+//    static void Main(string[] args)
+//    {
+//        FileRead_FH fh = new FileRead_FH();
+//        fh.read();
+//    }
+//}
+
