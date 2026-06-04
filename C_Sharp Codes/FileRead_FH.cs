4ebfdd0ec38519aa0c56aebed2c1defc0462b9fb
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-06-04 23:25:41 +0530

    Update FileRead_FH.cs

--- C_Sharp Codes/FileRead_FH.cs
+++ C_Sharp Codes/FileRead_FH.cs
@@ -1,28 +1,28 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 
-//class FileRead_FH
-//{
-//    void read()
-//    {
-//        string path = "D:\\VisualStudio\\Csharp\\Csharp_programing\\Csharp_programing\\newfile_FH.txt";
-//        FileStream fs = new FileStream(path, FileMode.OpenOrCreate);
+class FileRead_FH
+{
+   void read()
+   {
+       string path = "D:\\VisualStudio\\Csharp\\Csharp_programing\\Csharp_programing\\newfile_FH.txt";
+       FileStream fs = new FileStream(path, FileMode.OpenOrCreate);
 
-//        StreamReader sr = new StreamReader(fs);
-//        string alldata = sr.ReadToEnd();
-//        Console.WriteLine("data read done !");
-//        Console.WriteLine("the data is :");
-//        Console.WriteLine(alldata);
-//        sr.Close();
-//        fs.Close();
+       StreamReader sr = new StreamReader(fs);
+       string alldata = sr.ReadToEnd();
+       Console.WriteLine("data read done !");
+       Console.WriteLine("the data is :");
+       Console.WriteLine(alldata);
+       sr.Close();
+       fs.Close();
 
-//    }
-//    static void Main(string[] args)
-//    {
-//        FileRead_FH fh = new FileRead_FH();
-//        fh.read();
-//    }
-//}
+   }
+   static void Main(string[] args)
+   {
+       FileRead_FH fh = new FileRead_FH();
+       fh.read();
+   }
+}
 
