2171bc21d9da56e7dddca35753169fb0c907c594
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-06-04 23:24:45 +0530

    Update All_Oops.cs

--- C_Sharp Codes/All_Oops.cs
+++ C_Sharp Codes/All_Oops.cs
@@ -1,57 +1,57 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-//using System;
+using System;
 
-//// ABSTRACTION
-//abstract class Animal
-//{
-//    // ENCAPSULATION (private  property)
-//    private string Name;
+// ABSTRACTION
+abstract class Animal
+{
+   // ENCAPSULATION (private  property)
+   private string Name;
 
-//    public abstract void Sound();
+   public abstract void Sound();
 
-//    public void setname(string Name)
-//    {
-//        this.Name = Name;
-//    }
+   public void setname(string Name)
+   {
+       this.Name = Name;
+   }
 
-//    public string getname()
-//    {
-//        return Name;
-//    }
+   public string getname()
+   {
+       return Name;
+   }
 
-//    // Abstract method
+   // Abstract method
    
 
    
-//}
+}
 
-//// inheritance and polymorphism
-//class Dog : Animal
-//{
-//    public override void Sound()
-//    {
+// inheritance and polymorphism
+class Dog : Animal
+{
+   public override void Sound()
+   {
         
-//        Console.WriteLine(getname()+ " is Barking");
-//    }
-//}
+       Console.WriteLine(getname()+ " is Barking");
+   }
+}
 
-//class All_Oops
-//{
-//    static void Main()
-//    {
-//        // Object of child class
-//        Dog d = new Dog();
+class All_Oops
+{
+   static void Main()
+   {
+       // Object of child class
+       Dog d = new Dog();
 
-//        // Encapsulation (set value)
-//        d.setname("tommy");
+       // Encapsulation (set value)
+       d.setname("tommy");
 
-//        // Encapsulation (get output)
-//        Console.WriteLine("name is "+d.getname());
+       // Encapsulation (get output)
+       Console.WriteLine("name is "+d.getname());
 
-//        d.Sound(); //call polymorphism
+       d.Sound(); //call polymorphism
           
-//    }
-//}
+   }
+}
