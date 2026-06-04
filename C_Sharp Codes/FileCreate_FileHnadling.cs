e1c13a2c0118403fcfb9e333fe1edc2521ace1c8
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-06-04 23:25:23 +0530

    Update FileCreate_FileHnadling.cs

--- C_Sharp Codes/FileCreate_FileHnadling.cs
+++ C_Sharp Codes/FileCreate_FileHnadling.cs
@@ -1,31 +1,31 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 
-//   class FileCreate_FileHnadling
-//    {
-//    void myfile()
-//    {
+  class FileCreate_FileHnadling
+   {
+   void myfile()
+   {
        
 
-//        string path = "D:\\VisualStudio\\Csharp\\Csharp_programing\\Csharp_programing\\wednesday.txt";
-//        File.Create(path);
-//        if(File.Exists(path) )
-//        {
-//            Console.WriteLine("file created succesfully");
-//        }
-//        else
-//        {
-//            Console.WriteLine("file created failed");
-//        }
-//    }
+       string path = "D:\\VisualStudio\\Csharp\\Csharp_programing\\Csharp_programing\\wednesday.txt";
+       File.Create(path);
+       if(File.Exists(path) )
+       {
+           Console.WriteLine("file created succesfully");
+       }
+       else
+       {
+           Console.WriteLine("file created failed");
+       }
+   }
 
-//    static void Main(string[] args)
-//    {
-//        FileCreate_FileHnadling sc= new FileCreate_FileHnadling();
-//        sc.myfile();
-//    }
+   static void Main(string[] args)
+   {
+       FileCreate_FileHnadling sc= new FileCreate_FileHnadling();
+       sc.myfile();
+   }
 
-//}
+}
 
