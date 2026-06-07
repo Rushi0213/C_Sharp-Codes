fcb547b43b3239fa0aa15f40aa2c49a2fd4b48a4
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-06-07 21:49:51 +0530

    Update streamWriter_FH.cs

--- C_Sharp Codes/streamWriter_FH.cs
+++ C_Sharp Codes/streamWriter_FH.cs
@@ -1,29 +1,29 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 
-//class streamWriter_FH
-//{
+class streamWriter_FH
+{
 
-//    void show()
-//    {
-//        string path = "D:\\VisualStudio\\Csharp\\Csharp_programing\\Csharp_programing\\newfile_FH.txt";
-//        FileStream fs = new FileStream(path, FileMode.Create);
+   void show()
+   {
+       string path = "D:\\VisualStudio\\Csharp\\Csharp_programing\\Csharp_programing\\newfile_FH.txt";
+       FileStream fs = new FileStream(path, FileMode.Create);
 
-//        StreamWriter sw = new StreamWriter(fs);
+       StreamWriter sw = new StreamWriter(fs);
 
-//        sw.WriteLine("today is wednesday");
-//        sw.Write("happy wednesday");
-//        sw.Close();
-//        fs.Close();
-//        Console.WriteLine("file writed succesfully");
-//    }
+       sw.WriteLine("today is wednesday");
+       sw.Write("happy wednesday");
+       sw.Close();
+       fs.Close();
+       Console.WriteLine("file writed succesfully");
+   }
 
-//    static void Main(string[] args)
-//    {
-//        streamWriter_FH s = new streamWriter_FH();
-//        s.show();
-//    }
-//}
+   static void Main(string[] args)
+   {
+       streamWriter_FH s = new streamWriter_FH();
+       s.show();
+   }
+}
 
