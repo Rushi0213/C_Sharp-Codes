e40b9961994acff6eaf12c78988ab144887fd02a
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-06-07 21:49:18 +0530

    Update try_catch2.cs

--- C_Sharp Codes/try_catch2.cs
+++ C_Sharp Codes/try_catch2.cs
@@ -1,25 +1,25 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 
-//    class try_catch2
-//    {
-//    static void Main(string[] args)
-//    {
-//        string name = null;
+   class try_catch2
+   {
+   static void Main(string[] args)
+   {
+       string name = null;
 
-//        Console.WriteLine("string is :"+name);
+       Console.WriteLine("string is :"+name);
 
-//        try
-//        {
-//            Console.WriteLine("string length is:" + name.Length);
-//        }
-//        catch (Exception e)
-//        {
-//            Console.WriteLine("exception handled"+e);
-//        }
+       try
+       {
+           Console.WriteLine("string length is:" + name.Length);
+       }
+       catch (Exception e)
+       {
+           Console.WriteLine("exception handled"+e);
+       }
         
-//    }
-//}
+   }
+}
 
