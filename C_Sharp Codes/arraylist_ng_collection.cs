04eaf805349659dda53aa3f3312edec5c5d2e95d
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-06-09 20:30:55 +0530

    arraylist collection c#

---
+++ C_Sharp Codes/arraylist_ng_collection.cs
@@ -0,0 +1,37 @@
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Text;
+
+
+//    class arraylist_ng_collection
+//    {
+
+//    public void nongereric()
+//    {
+//        Console.WriteLine("non generic collection Ilist:arraylist");
+
+//        ArrayList list = new ArrayList();
+
+//        list.Add("rushi");
+//        list.Add(2000);
+//        list.Add(7.98);
+//        list.Add(7.98);
+//        //list.Remove(2000);
+//        list.Insert(1,3000);
+//        //list.Remove(2000);
+
+//        Console.WriteLine("array list is: \n");
+//        for(int i=0;i<list.Count;i++)
+//        {
+//            Console.WriteLine( list[i]);
+//        }
+//    }
+//    static void Main(string[] args)
+//    {
+//        arraylist_ng_collection a= new arraylist_ng_collection();
+
+//        a.nongereric();
+//    }
+//}
+
