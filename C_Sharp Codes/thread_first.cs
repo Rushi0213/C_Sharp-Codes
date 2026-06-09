d59547266d34f7142c81605d3195d388c1279c1c
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-06-09 20:33:24 +0530

    new code of thread

---
+++ C_Sharp Codes/thread_first.cs
@@ -0,0 +1,22 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+
+//     class thread_first
+//    {
+
+//      void add()
+//    {
+//        Console.WriteLine("this is thread method");
+//    }
+//    static void Main(string[] args)
+//    {
+
+//        thread_first s= new thread_first();
+//        Thread t = new Thread(s.add);
+//        t.Start();
+        
+//    }
+//}
+
