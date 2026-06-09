4a96fe52192884ebe81d88e5a278721f65e14b65
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-06-09 20:31:31 +0530

    second code arraylist collection c#

---
+++ C_Sharp Codes/arraylist1_nongeneric.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+
+    class arraylist1_nongeneric
+    {
+
+      public void add()
+    {
+        Console.WriteLine("arralist of empoyee detail ");
+
+        ArrayList a= new ArrayList();
+
+        a.Add("ram");
+        a.Add(12);
+        a.Add(35000);
+        a.Add(7822007538);
+        a.Add("ram@gmail.com");
+
+
+
+    }
+    static void Main(string[] args)
+    {
+        
+    }
+}
+
