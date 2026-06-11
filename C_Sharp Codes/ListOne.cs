30565b7c13725d9de7e7a0626a8648053e00e056
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-06-11 23:11:45 +0530

    list generic collection

---
+++ C_Sharp Codes/ListOne.cs
@@ -0,0 +1,32 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+
+//    class ListOne
+//    {
+
+//    void add()
+//    {
+//        List<string> students = new List<string>();
+
+//        students.Add("rushi");
+//        students.Add("madhav");
+//        students.Add("ram");
+//        students.Add("shyam");
+
+//        Console.WriteLine("list is: "); 
+//        foreach(var student in students)
+//        {
+//            Console.WriteLine(student);
+//        }
+//    }
+    
+//    static void Main(string[] args)
+//    {
+//        ListOne s = new ListOne();
+//        s.add();
+
+//    }
+//}
+
