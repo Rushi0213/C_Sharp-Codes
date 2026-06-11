009436315ed5c6b540737c6bae7092e4c1d7513e
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-06-11 23:16:45 +0530

    stack non generic collection

---
+++ C_Sharp Codes/stack_collection.cs
@@ -0,0 +1,56 @@
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Text;
+
+
+//class program
+//{
+
+//    //stack is the (last in first out) method in collection
+//    public void details()
+//    {
+//        Stack c= new Stack();
+//        c.Push("rushi");
+//        c.Push("ram");
+//        c.Push("shyam");
+//        c.Push("raju");
+
+//        Console.WriteLine(" name of studnet");
+//        foreach(var i in c)
+//        {
+//            Console.WriteLine(i);
+//        }
+//        Console.WriteLine();
+
+//        c.Pop();//removing elment from stack
+
+//        Console.WriteLine("after removing student");
+//        foreach (var i in c)
+//        {
+//            Console.WriteLine(i);
+//        }
+
+//        c.Pop();
+//        Console.WriteLine();
+//        Console.WriteLine("again removing one more");
+//        foreach (var i in c)
+//        {
+//            Console.WriteLine(i);
+//        }
+//    }
+//}
+//     class stack_collection
+//    {
+
+    
+//    static void Main(string[] args)
+//    {
+//        program p= new program();
+
+//        Thread t = new Thread(p.details);
+      
+//        t.Start();
+//    }
+//}
+
