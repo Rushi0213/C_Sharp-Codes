aa7b3011f8aec9685f8de140284893926e9b9558
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-06-11 23:15:17 +0530

    hashtable non generic collection

---
+++ C_Sharp Codes/Hashtable1.cs
@@ -0,0 +1,61 @@
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Text;
+
+
+
+
+//class program
+//{
+//    public void details()
+//    {
+
+//        Hashtable ht = new Hashtable();// structure of hashtable
+
+//        ht.Add(1, "rushi");
+//        ht.Add(4, "rahul");
+//        ht.Add(2, "ram");
+//        ht.Add(3, "raju");
+
+
+
+        
+
+//        foreach (var ht2 in ht)
+//        {
+//            Console.WriteLine(ht2);//for each loop run here
+
+
+//        }
+
+//        Console.WriteLine(ht.ContainsKey(1));// output in tru and false
+//        Console.WriteLine(ht.ContainsValue("rushi"));//output in true and false
+//        Console.WriteLine(ht.Contains(2));
+
+//        object ch = "rushi";
+//        int count = 0;
+
+//        for(int i=0;i<ht.Count; i++)
+//        {
+//            if (
+//            {
+
+//            }
+//        }
+//    }
+//}
+
+//class Hashtable1
+//{
+//    static void Main(string[] args)
+//    {
+//        program p = new program();// object of class
+
+//        Thread t = new Thread(p.details);//object of thread with call details methods
+
+//        t.Start();//call thread method start
+
+//    }
+//}
+
