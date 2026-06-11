d00405abdbd369ff23e3d9d866552703117e7c4d
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-06-11 23:10:17 +0530

    nongeneric arralist collection

--- C_Sharp Codes/arraylist1_nongeneric.cs
+++ C_Sharp Codes/arraylist1_nongeneric.cs
@@ -1,30 +1,36 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Text;
 
 
-    class arraylist1_nongeneric
-    {
+//    class arraylist1_nongeneric
+//    {
 
-      public void add()
-    {
-        Console.WriteLine("arralist of empoyee detail ");
+//      public void add()
+//    {
+//        Console.WriteLine("arralist of empoyee detail ");
 
-        ArrayList a= new ArrayList();
+//        ArrayList a= new ArrayList();
 
-        a.Add("ram");
-        a.Add(12);
-        a.Add(35000);
-        a.Add(7822007538);
-        a.Add("ram@gmail.com");
+//        a.Add("ram");
+//        a.Add(12);
+//        a.Add(35000);
+//        a.Add(7822007538);
+//        a.Add("ram@gmail.com");
 
+//Console.WriteLine("array list is: \n");
+//        for(int i=0;i<list.Count;i++)
+//        {
+//            Console.WriteLine( list[i]);
+//        }
 
+//    }
+//    static void Main(string[] args)
+//    {
+//arraylist1_nongeneric a = new arraylist1_nongeneric();
 
-    }
-    static void Main(string[] args)
-    {
-        
-    }
-}
+//        a.add();
+//    }
+//}
 
