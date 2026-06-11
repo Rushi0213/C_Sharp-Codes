f1fc3379f58b3633398286b5c6c4eb3b8aa00d01
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-06-11 23:13:50 +0530

    dictionary generic  key and value with collection

---
+++ C_Sharp Codes/dictionary_keyValue_collection.cs
@@ -0,0 +1,54 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//    class dictionary_keyValue_collection
+//    {
+//    public void details()
+//    {
+          //dictionary is same as a hashmap in java
+//        Console.WriteLine("company info");
+
+
+//        //dictionary stores unique key but can store duplicate value
+//        Dictionary<Int32, String> dic = new Dictionary<Int32, String>();
+
+//        dic.Add(1, "microsoft");
+//        dic.Add(2, "google");
+//        dic.Add(3, "amazon");
+//        dic.Add(4, "cognizant");
+//        dic.Add(5, "cognizant");
+
+
+//        foreach (var v in dic)
+//        {
+           
+//            Console.WriteLine(v);
+//            Thread.Sleep(1000);
+//        }
+
+//        //this is for keypair
+//        foreach(Int32 v in dic.Keys)
+//        {
+//            Console.WriteLine(v);
+//            Thread.Sleep(1000);
+//        }
+
+
+//        //this is for value pair
+//        foreach (String n in dic.Values)
+//        {
+//            Console.WriteLine(n);
+//            Thread.Sleep(1000);
+//        }
+
+
+
+//    }
+//        static void Main(string[] args)
+//    {
+//        dictionary_keyValue_collection d= new dictionary_keyValue_collection();
+//        d.details();
+//    }
+//}
+
