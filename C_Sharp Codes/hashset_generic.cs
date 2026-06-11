5892f89acced529ab629c619aef87635c13828e5
Rushikesh Deshmukh <156175551+Rushi0213@users.noreply.github.com>
2026-06-11 23:14:39 +0530

    hashset generic collection

---
+++ C_Sharp Codes/hashset_generic.cs
@@ -0,0 +1,34 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+
+//class program
+//{
+
+//    public void details()
+//    {
+//         HashSet<string> email = new HashSet<string>();
+
+//        email.Add("rushi@gmail.com");
+//        email.Add("aditi@gmail.com");
+//        email.Add("pralhad@gmail.com");
+//        email.Add("sandesh@gmail.com");
+//        email.Add("aditi@gmail.com");
+
+//        foreach(string s in email)
+//        {
+//            Console.WriteLine(s);
+//        }
+//    }
+//}
+
+//    class hashset_generic
+//    {
+//    static void Main(string[] args)
+//    {
+//     program p= new program();
+//        p.details();
+//    }
+//}
+
